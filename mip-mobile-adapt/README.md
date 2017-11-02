aa694a3752a19a2d894fd25c1eac34e5fa98acf7
edwin <sblig@126.com>
2017-11-02 13:19:24 +0800

    FECS 校验通过

--- mip-mobile-adapt/README.md
+++ mip-mobile-adapt/README.md
@@ -1,13 +1,24 @@
 # mip-mobile-adapt
 
-样式尺寸转换
+mip-mobile-adapt 页面样式尺寸转换
 
-描述|页面尺寸转换
+标题|内容
 ----|----
-类型|事件
-支持布局| N/S
+类型|通用
+支持布局|container
 所需脚本|https://mipcache.bdstatic.com/static/v1/mip-mobile-adapt/mip-mobile-adapt.js
 
 ## 示例
 
+```html
+<mip-mobile-adapt></mip-mobile-adapt>
+```
 
+## 属性
+
+### mip-mobile-adapt
+
+说明：页面样式尺寸转换
+必选项：无  
+类型：无  
+格式：无
--- mip-mobile-adapt/mip-mobile-adapt.js
+++ mip-mobile-adapt/mip-mobile-adapt.js
@@ -3,23 +3,23 @@
  * @author edwin
  */
 define(function (require) {
-	var docEl = document.documentElement,
-		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-		recalc = function () {
-			var clientWidth = docEl.clientWidth;
-			if (!clientWidth) return;
-			if(clientWidth>=640){
-				docEl.style.fontSize = '100px';
-			}else{
-				docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
-			}
-		};
+    var docEl = document.documentElement;
+    var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    var recalc = function () {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) {
+            return;
+        }
+        if (clientWidth >= 640) {
+            docEl.style.fontSize = '100px';
+        } else {
+            docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+        }
+    };
 
-	if (!document.addEventListener) return;
-	window.addEventListener(resizeEvt, recalc, false);
-	document.addEventListener('DOMContentLoaded', recalc, false);
-
-
+    if (!document.addEventListener) {
+        return;
+    }
+    window.addEventListener(resizeEvt, recalc, false);
+    document.addEventListener('DOMContentLoaded', recalc, false);
 });
-
-

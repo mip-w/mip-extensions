af7c7514877bbeeb73bf712527767ce53b2813ec
edwin <sblig@126.com>
2017-11-02 11:32:05 +0800

    样式尺寸转换

---
+++ mip-mobile-adapt/README.md
@@ -0,0 +1,13 @@
+# mip-mobile-adapt
+
+样式尺寸转换
+
+描述|页面尺寸转换
+----|----
+类型|事件
+支持布局| N/S
+所需脚本|https://mipcache.bdstatic.com/static/v1/mip-mobile-adapt/mip-mobile-adapt.js
+
+## 示例
+
+
---
+++ mip-mobile-adapt/mip-mobile-adapt.js
@@ -0,0 +1,25 @@
+/**
+ * @file mobile-adapt
+ * @author edwin
+ */
+define(function (require) {
+	var docEl = document.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function () {
+			var clientWidth = docEl.clientWidth;
+			if (!clientWidth) return;
+			if(clientWidth>=640){
+				docEl.style.fontSize = '100px';
+			}else{
+				docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+			}
+		};
+
+	if (!document.addEventListener) return;
+	window.addEventListener(resizeEvt, recalc, false);
+	document.addEventListener('DOMContentLoaded', recalc, false);
+
+
+});
+
+
